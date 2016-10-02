d4322970678fec9a56534cee196e6979e2efec4f
Neha Singh <nehasingh18.9@gmail.com>
2016-10-02 17:52:39 -0400

    put assignemnt in correct folder

---
+++ public/assignment/js/Assignment2.js
@@ -0,0 +1,143 @@
+function RedirectToRegistration()
+{
+    window.open('Register.html', '_self');
+}
+
+function RedirectToLogin()
+{
+    window.open('Login.html', '_self');
+}
+
+function RedirectToWishList()
+{
+    window.open('website-list.html', '_self');
+}
+
+function RedirectToWidgetList() {
+    window.open('widget-list.html', '_self');
+}
+
+function RedirectToPageList() {
+    window.open('Page-list.html', '_self');
+}
+
+function ValidateLogin()
+{
+    $('#form1').bootstrapValidator({
+        //container: '#messages',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        }, //end of feedbackIcons
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'Username is required.'
+                    }
+                }
+            },
+            inputPassword: {
+                validators: {
+                    notEmpty: {
+                        message: 'Password is required.'
+                    }
+                }
+            }
+        } // end of fields
+        //onSuccess: function (e, data) {
+        //    alert('Success');
+        //    window.open('../User/Profile.html', '_self');
+        //}
+    });// end of form1
+}
+
+function ValidateRegistration()
+{
+    $('#form1').bootstrapValidator({
+        //container: '#messages',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        }, //end of feedbackIcons
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'Username is required.'
+                    }
+                }
+            },
+            inputPassword: {
+                validators: {
+                    notEmpty: {
+                        message: 'Password is required.'
+                    },
+                    identical: {
+                        field: 'verifyPassword',
+                        message: 'The password and its confirm are not the same'
+                    }
+                }
+            },
+            verifyPassword: {
+                validators: {
+                    notEmpty: {
+                        message: 'Verify password is required.'
+                    },
+                    identical: {
+                        field: 'inputPassword',
+                        message: 'The password and its confirm are not the same'
+                    }
+                }
+            },
+        } // end of fields
+    });// end of form1
+}
+
+function ValidateProfile()
+{
+    $('#form1').bootstrapValidator({
+        //container: '#messages',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        }, //end of feedbackIcons
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'Username is required.'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Email address is required.'
+                    }
+                }
+            },
+            firstName: {
+                validators: {
+                    notEmpty: {
+                        message: 'First name is required.'
+                    }
+                }
+            },
+            lastName: {
+                validators: {
+                    notEmpty: {
+                        message: 'Last name is required.'
+                    }
+                }
+            }
+        } // end of fields
+        //onSuccess: function (e, data) {
+        //    alert('Success');
+        //    window.open('../User/Profile.html', '_self');
+        //}
+    });// end of form1
+}
